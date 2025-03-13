5eb5369d0ecf2cdefd304d08f2cb3134af1ed755
François Margall <fr.margall@proton.me>
2025-03-13 12:03:21 +0100

    bulid: define RIVOLI.App as start-up project in Visual Studio

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1,7 +1,13 @@
-#include <string>
+#include <fstream>
 
 #include "configurator.hpp"
+#include "logger.hpp"
 
 MainConfig::MainConfig(const std::string& configFilePath) {
-
+	// Open the configuration file
+	std::ifstream configFile(configFilePath);
+	if (configFile.is_open())
+		LOG_DEBUG("Configuration file ", configFilePath, " opened.");
+	else
+		LOG_CRITICAL("Configuration file ", configFilePath, " could not be opened.");
 }
