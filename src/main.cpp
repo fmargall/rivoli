eb69fa8ea8e54d9e9a379cb6ad690c513b6a31dc
François Margall <fr.margall@proton.me>
2025-03-14 15:53:24 +0100

    refactor: RuntimeConfig in main.cpp passed to polymporphism

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,19 @@ int main(int argc, char* argv[]) {
 	std::string configFilePath = argv[1];
 	MainConfig mainConfig(configFilePath);
 
+	std::string floatingPointPrecision = mainConfig.getFloatingPointPrecision();
 
+	// Initialisation of the runtime configuration
+	std::unique_ptr<MainConfig> runtimeConfig;
 
+	// Selecting numerical precision (FP32 for single and FP64 for double precision)
+	if      (floatingPointPrecision == "FP32")
+		runtimeConfig = std::make_unique<RuntimeConfig<float>>(mainConfig);
+	else if (floatingPointPrecision == "FP64")
+		runtimeConfig = std::make_unique<RuntimeConfig<double>>(mainConfig);
+	else
+		LOG_CRITICAL("Unknown floating point precision: ", floatingPointPrecision, ". Should be 'FP32' or 'FP64' only.");
+
+	LOG_TRACE("RIVOLI program will end.");
 	return 0;
 }
