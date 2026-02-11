2c805f28600a7efdf1d5d9b1601940a81602af50
François Margall <fr.margall@proton.me>
2026-02-11 16:58:49 +0100

    ✨ feat(distances): add 3D distances functions

--- include/rivoli/distances/distances.hpp
+++ include/rivoli/distances/distances.hpp
@@ -18,13 +18,92 @@ FORCE_INLINE vectra::Vectratype<FP, level> distanceGreatCircle(
                      vct::cos(thetaOne) * vct::cos(thetaTwo) * vct::cos(phiOne - phiTwo));
 }
 
+template <typename FP, vectra::SIMDLevel level>
+FORCE_INLINE vectra::Vectratype<FP, level> distance1Rx2SEuclidean(
+    vectra::Vectratype<FP, level> thetaAOne,
+    vectra::Vectratype<FP, level> thetaBOne,
+    vectra::Vectratype<FP, level> phiBOne  ,
+    vectra::Vectratype<FP, level> thetaATwo,
+    vectra::Vectratype<FP, level> thetaBTwo,
+    vectra::Vectratype<FP, level> phiBTwo)
+{
+    using vct = vectra::Vectratype<FP, level>;
+
+    vct distR1 = thetaAOne - thetaATwo;
+    vct distS2 = rivoli::distanceGreatCircle(thetaBOne, phiBOne, thetaBTwo, phiBTwo);
+
+    return vct::sqrt(distR1 * distR1 + distS2 * distS2);
+}
+
+template <typename FP, vectra::SIMDLevel level>
+FORCE_INLINE vectra::Vectratype<FP, level> distance1Rx2SBilateralEuclidean(
+    vectra::Vectratype<FP, level> thetaAOne,
+    vectra::Vectratype<FP, level> thetaBOne,
+    vectra::Vectratype<FP, level> phiBOne  ,
+    vectra::Vectratype<FP, level> thetaATwo,
+    vectra::Vectratype<FP, level> thetaBTwo,
+    vectra::Vectratype<FP, level> phiBTwo)
+{
+    using vct = vectra::Vectratype<FP, level>;
+
+    vct distanceOne = rivoli::distance1Rx2SEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                                     thetaATwo, thetaBTwo, phiBTwo);
+    vct distanceTwo = rivoli::distance1Rx2SEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                     thetaATwo, thetaBTwo, vct::two_pi() - phiBTwo);
+
+    return vct::min(distanceOne, distanceTwo);
+}
+
+template <typename FP, vectra::SIMDLevel level>
+FORCE_INLINE vectra::Vectratype<FP, level> distance1Rx2SReciprocalEuclidean(
+    vectra::Vectratype<FP, level> thetaAOne,
+    vectra::Vectratype<FP, level> thetaBOne,
+    vectra::Vectratype<FP, level> phiBOne  ,
+    vectra::Vectratype<FP, level> thetaATwo,
+    vectra::Vectratype<FP, level> thetaBTwo,
+    vectra::Vectratype<FP, level> phiBTwo)
+{
+    using vct = vectra::Vectratype<FP, level>;
+
+    vct distanceOne   = rivoli::distance1Rx2SEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                                       thetaATwo, thetaBTwo, phiBTwo);
+    vct distanceTwo   = rivoli::distance1Rx2SEuclidean(thetaBOne, thetaAOne, vct::two_pi() - phiBOne,
+                                                       thetaATwo, thetaBTwo, phiBTwo);
+    vct distanceThree = rivoli::distance1Rx2SEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                                       thetaBTwo, thetaATwo, vct::two_pi() - phiBTwo);
+    vct distanceFour  = rivoli::distance1Rx2SEuclidean(thetaBOne, thetaAOne, vct::two_pi() - phiBOne,
+                                                       thetaBTwo, thetaATwo, vct::two_pi() - phiBTwo);
+
+    return vct::min(vct::min(distanceOne, distanceTwo), vct::min(distanceThree, distanceFour));
+}
+
+template <typename FP, vectra::SIMDLevel level>
+FORCE_INLINE vectra::Vectratype<FP, level> distance1Rx2SBilateralReciprocalEuclidean(
+    vectra::Vectratype<FP, level> thetaAOne,
+    vectra::Vectratype<FP, level> thetaBOne,
+    vectra::Vectratype<FP, level> phiBOne  ,
+    vectra::Vectratype<FP, level> thetaATwo,
+    vectra::Vectratype<FP, level> thetaBTwo,
+    vectra::Vectratype<FP, level> phiBTwo)
+{
+    using vct = vectra::Vectratype<FP, level>;
+
+    vct distanceOne = rivoli::distance1Rx2SReciprocalEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                                     thetaATwo, thetaBTwo, phiBTwo);
+    vct distanceTwo = rivoli::distance1Rx2SReciprocalEuclidean(thetaAOne, thetaBOne, phiBOne,
+                                                               thetaATwo, thetaBTwo, vct::two_pi() - phiBTwo);
+
+    return vct::min(distanceOne, distanceTwo);
+}
+
+
 namespace experimental
 {
 
 template <typename FP, vectra::SIMDLevel level>
 FORCE_INLINE  typename vectra::ComputeBackend<FP, level>::type distanceGreatCircle(
     const typename vectra::ComputeBackend<FP, level>::type& thetaOne,
-    const typename vectra::ComputeBackend<FP, level>::type& phiOne,
+    const typename vectra::ComputeBackend<FP, level>::type& phiOne  ,
     const typename vectra::ComputeBackend<FP, level>::type& thetaTwo,
     const typename vectra::ComputeBackend<FP, level>::type& phiTwo)
 {
