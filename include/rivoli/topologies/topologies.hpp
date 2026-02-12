60958a2dced0939b37f213b75fdb62cc4214605a
François Margall <fr.margall@proton.me>
2026-02-12 10:18:08 +0100

    ✨ feat(topologies): add 2-sphere topology

--- include/rivoli/topologies/topologies.hpp
+++ include/rivoli/topologies/topologies.hpp
@@ -2,6 +2,12 @@
 
 #include <vectra/vectra.hpp>
 
+#include <rivoli/distances/distances.hpp>
+
+namespace rivoli
+{
+
+
 template <typename _FP, vectra::SIMDLevel _level, typename DerivedTopology>
 class Topology {
 public:
@@ -11,8 +17,32 @@ public:
 	using vct = vectra::Vectratype<FP, level>;
 
 	template <typename... Args>
-	vct getDistance(Args&&... args) const {
+	FORCE_INLINE vct getDistance(Args&&... args) const {
 		return static_cast<const DerivedTopology*>(this)->_getDistance(std::forward<Args>(args)...);
 	}
 
 };
+
+template <typename _FP, vectra::SIMDLevel _level>
+class Topology2S : public Topology<_FP, _level, Topology2S<_FP, _level>> {
+
+	template <typename _FPAlias, vectra::SIMDLevel _levelAlias, typename DerivedTopology>
+	friend class Topology;
+
+public:
+	using FP = _FP;
+	static constexpr vectra::SIMDLevel level = _level;
+	static constexpr std::size_t dimension   = 2;
+
+protected:
+	using vct = vectra::Vectratype<FP, level>;
+
+	FORCE_INLINE static vct _getDistance(vct thetaOne, vct phiOne,
+										 vct thetaTwo, vct phiTwo)
+	{
+		return rivoli::distanceGreatCircle(thetaOne, phiOne, thetaTwo, phiTwo);
+	}
+};
+
+
+}
