6a47d00679f308b26824c77589a4eddd8c5ec24e
François Margall <fr.margall@proton.me>
2026-02-12 10:20:32 +0100

    ⚗️ chore(benchmark): add Topology2S in distance benchmark

--- benchmark/benchmark_distances.cpp
+++ benchmark/benchmark_distances.cpp
@@ -2,7 +2,26 @@
 
 #include <rivoli/rivoli.hpp>
 
-static void BM_distance_vectratype_Nonef(benchmark::State& state)
+static void BM_2S_topology_vectratype_Nonef(benchmark::State& state)
+{
+    rivoli::Topology2S<float, vectra::SIMDLevel::None> topology;
+
+    using vct = vectra::Vectratype<float, vectra::SIMDLevel::None>;
+
+    vct t1(0.1f), p1(0.2f), t2(0.3f), p2(0.4f);
+
+    for (auto _ : state)
+    {
+        auto r = topology.getDistance(t1, p1, t2, p2);
+        benchmark::DoNotOptimize(r);
+    }
+}
+BENCHMARK(BM_2S_topology_vectratype_Nonef)
+    ->MinTime(60.0)
+    ->Repetitions(10)
+    ->ReportAggregatesOnly();
+
+static void BM_2S_distance_vectratype_Nonef(benchmark::State& state)
 {
     using vct = vectra::Vectratype<float, vectra::SIMDLevel::None>;
 
@@ -14,9 +33,12 @@ static void BM_distance_vectratype_Nonef(benchmark::State& state)
         benchmark::DoNotOptimize(r);
     }
 }
-BENCHMARK(BM_distance_vectratype_Nonef)->MinTime(15.0);
+BENCHMARK(BM_2S_distance_vectratype_Nonef)
+    ->MinTime(60.0)
+    ->Repetitions(10)
+    ->ReportAggregatesOnly();
 
-static void BM_distance_backend_Nonef(benchmark::State& state)
+static void BM_2S_distance_backend_Nonef(benchmark::State& state)
 {
     using backend = vectra::ComputeBackend<float, vectra::SIMDLevel::None>;
     using T = typename backend::type;
@@ -29,6 +51,9 @@ static void BM_distance_backend_Nonef(benchmark::State& state)
         benchmark::DoNotOptimize(r);
     }
 }
-BENCHMARK(BM_distance_backend_Nonef)->MinTime(15.0);
+BENCHMARK(BM_2S_distance_backend_Nonef)
+    ->MinTime(60.0)
+    ->Repetitions(10)
+    ->ReportAggregatesOnly();
 
 BENCHMARK_MAIN();
