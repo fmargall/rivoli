9bd5ff031145b5f9914f1f49494ea8661312042c
François Margall <fr.margall@proton.me>
2026-02-12 10:28:01 +0100

    ✨ feat(topologies): add 2S bilateral topology

--- include/rivoli/topologies/topologies.hpp
+++ include/rivoli/topologies/topologies.hpp
@@ -44,5 +44,25 @@ protected:
 	}
 };
 
+template <typename _FP, vectra::SIMDLevel _level>
+class Topology2SBilateral : public Topology<_FP, _level, Topology2SBilateral<_FP, _level>> {
+
+	template <typename _FPAlias, vectra::SIMDLevel _levelAlias, typename DerivedTopology>
+	friend class Topology;
+
+public:
+	using FP = _FP;
+	static constexpr vectra::SIMDLevel level = _level;
+	static constexpr std::size_t dimension   = 2;
+
+protected:
+	using vct = vectra::Vectratype<FP, level>;
+
+	FORCE_INLINE static vct _getDistance(vct thetaOne, vct phiOne,
+										 vct thetaTwo, vct phiTwo)
+	{
+		return rivoli::distanceGreatCircleBilateral(thetaOne, phiOne, thetaTwo, phiTwo);
+	}
+};
 
 }
