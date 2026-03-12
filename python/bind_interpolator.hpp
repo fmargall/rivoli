e737bad71ef81d3b0e5c5321dd12bb6e32121edd
François Margall <fr.margall@proton.me>
2026-03-12 09:51:16 +0100

    ✨ feat: add automatic naming for interpolator binding

--- python/bind_interpolator.hpp
+++ python/bind_interpolator.hpp
@@ -9,16 +9,29 @@ namespace rivoli {
 
 
 template <typename FP, vectra::SIMDLevel level, size_t dim, bool bilateral, bool reciprocal, KernelType kernelType>
-void bindInterpolator(nb::module_& m, const char* name) {
+void bindInterpolator(nb::module_& m) {
 
 	using KernelClass   = rivoli::KernelSelector<kernelType, FP, level>;
 	using TopologyClass = rivoli::TopologySelector<dim, bilateral, reciprocal, FP, level>;
 
 	using InterpolatorClass = Interpolator<KernelClass, TopologyClass>;
-	
-	nb::class_<InterpolatorClass> cls(m, name);
 
-	if constexpr (dim == 2) {
+	// Definition of the interpolator name
+	std::string name = "_Interpolator_";
+
+	name += std::string(TopologyClass::name);
+	name += "_";
+
+	name += std::string(KernelClass::name);
+	name += "_";
+
+	name += vectra::toString(level);
+
+	name += std::is_same_v<FP, float> ? "_f32" : "_f64";
+	
+	nb::class_<InterpolatorClass> cls(m, name.c_str());
+
+	if      constexpr (dim == 2) {
 		cls.def("interpolate", [](const InterpolatorClass& self, FP theta, FP phi) {
 			return self.interpolate(theta, phi);
 		});
--- python/binding.cpp
+++ python/binding.cpp
@@ -11,20 +11,20 @@ template<typename Enum, Enum... Values>
 struct value_list {};
 
 using SIMDLevels = value_list<
-    vectra::SIMDLevel, // Mandatory to expose type of enum before the values
-    vectra::SIMDLevel::None,
-    vectra::SIMDLevel::SSE,
-    vectra::SIMDLevel::SSE2,
-    vectra::SIMDLevel::SSE3,
-    vectra::SIMDLevel::SSE41,
-    vectra::SIMDLevel::SSE42,
-    vectra::SIMDLevel::AVX,
-    vectra::SIMDLevel::AVX2,
+    vectra::SIMDLevel        , // Mandatory to expose type of enum before the values
+    vectra::SIMDLevel::None  ,
+    vectra::SIMDLevel::SSE   ,
+    vectra::SIMDLevel::SSE2  ,
+    vectra::SIMDLevel::SSE3  ,
+    vectra::SIMDLevel::SSE41 ,
+    vectra::SIMDLevel::SSE42 ,
+    vectra::SIMDLevel::AVX   ,
+    vectra::SIMDLevel::AVX2  ,
     vectra::SIMDLevel::AVX512
 >;
 
 using KernelTypes = value_list<
-    rivoli::KernelType, // Mandatory to expose type of enum before the values
+    rivoli::KernelType        , // Mandatory to expose type of enum before the values
     rivoli::KernelType::Linear
 >;
 
@@ -44,26 +44,40 @@ NB_MODULE(_binding, m) {
 
 	// Compile-time, for-loop over FP types
 	[&] <typename... FPs>(type_list<FPs...>) {
+        ([&] {
+            using FP = FPs;
+            
+            // Compile-time for-loop over SIMD levels
+            [&] <vectra::SIMDLevel... Levels>(value_list<vectra::SIMDLevel, Levels...>) {
+                ([&] {
+                    constexpr auto level = Levels;
 
-        // Compile-time for-loop over SIMD levels
-        [&] <vectra::SIMDLevel... Levels>(value_list<vectra::SIMDLevel, Levels...>) {
+                    // Compile-time for-loop over kernels
+                    [&] <rivoli::KernelType... Ks>(value_list<rivoli::KernelType, Ks...>) {
+                        ([&] {
+                            constexpr auto kernelType = Ks;
 
-            // Compile-time for-loop over kernels
-            [&] <rivoli::KernelType... Ks>(value_list<rivoli::KernelType, Ks...>) {
-                
-                // 
-                [&] <typename... Topologies>(type_list<Topologies...>) {
-                    // Do something here
-                }(TopologyTags{});
+                            // Compile-time for-loop over topologies
+                            [&] <typename... Topologies>(type_list<Topologies...>) {
+                                ([&] {
+                                    using Topology = Topologies;
 
+                                    constexpr size_t dim      = Topology::dimension;
+                                    constexpr bool bilateral  = Topology::bilateral;
+                                    constexpr bool reciprocal = Topology::reciprocal;
 
-            }(KernelTypes{});
+                                    bindInterpolator<FP, level, dim, bilateral, reciprocal, kernelType>(m);
 
-        }(SIMDLevels{});
+                                }(), ...);
+                            }(TopologyTypes{});
 
-		
+                        }(), ...);
+                    }(KernelTypes{});
+
+                }(), ...);
+            }(SIMDLevels{});
+
+        }(), ...);
 	}(type_list<float, double>{});
-
-
 
 }
