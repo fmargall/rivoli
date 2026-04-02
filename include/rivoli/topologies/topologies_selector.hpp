13153a4526d4fcac848e48420aae4824b3ae0eb4
François Margall <fr.margall@proton.me>
2026-04-02 11:34:33 +0200

    ✨ feat(topologies): add topology selectors for Rusinkiewicz coordinates

--- include/rivoli/topologies/topologies_selector.hpp
+++ include/rivoli/topologies/topologies_selector.hpp
@@ -50,4 +50,31 @@ struct TopologyTraits<4, true, true, CoordinateSystem::Spherical, FP, level> {
 	using type = Topology2Sx2SBilateralReciprocalEuclidean<FP, level>;
 };
 
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<4, false, false, CoordinateSystem::Rusinkiewicz, FP, level> {
+	// Fallback to the usual 4D topology, since the
+	// Rusinkiewicz system is also defined as 2Sx2S
+	using type = Topology2Sx2SEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<4, false, true, CoordinateSystem::Rusinkiewicz, FP, level> {
+	using type = Topology2Sx2SReciprocalRusinkiewiczEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<4, true, false, CoordinateSystem::Rusinkiewicz, FP, level> {
+	// Rusinkiewicz parameterisation cannot intrinsically force
+	// symmetry, without forcing also the Helmholtz reciprocity
+	static_assert(false, "Rusinkiewicz parameterisation cannot be bilateral symmetrical without being also reciprocal");
+	using type = Topology2Sx2SBilateralReciprocalRusinkiewiczEuclidean<FP, level>;
+
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<4, true, true, CoordinateSystem::Rusinkiewicz, FP, level> {
+	using type = Topology2Sx2SBilateralReciprocalRusinkiewiczEuclidean<FP, level>;
+};
+
 }
