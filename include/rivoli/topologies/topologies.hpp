e4c8354c04a5a92ba01d2e0f9417f2cd0ecbb7f9
François Margall <fr.margall@proton.me>
2026-04-08 09:47:57 +0200

    ✨ feat(topologies): add 1Rx2S bilateral reciprocal euclidean Rusinkiewicz topology

--- include/rivoli/topologies/topologies.hpp
+++ include/rivoli/topologies/topologies.hpp
@@ -183,6 +183,29 @@ protected:
 };
 
 template <typename _FP, vectra::SIMDLevel _level>
+class Topology1Rx2SBilateralReciprocalRusinkiewiczEuclidean : public Topology<_FP, _level, Topology1Rx2SBilateralReciprocalRusinkiewiczEuclidean<_FP, _level>> {
+
+	template <typename _FPAlias, vectra::SIMDLevel _levelAlias, typename DerivedTopology>
+	friend class Topology;
+
+public:
+	using FP = _FP;
+	static constexpr vectra::SIMDLevel level = _level;
+	static constexpr std::size_t dimension = 3;
+	static constexpr std::string_view name = "1Rx2SBilateralReciprocalRusinkiewiczEuclidean";
+
+protected:
+	using vct = vectra::Vectratype<FP, level>;
+
+	FORCE_INLINE static vct _getDistance(vct thetaAOne, vct thetaBOne, vct phiBOne,
+										 vct thetaATwo, vct thetaBTwo, vct phiBTwo)
+	{
+		return rivoli::distance1Rx2SBilateralReciprocalRusinkiewiczEuclidean(thetaAOne, thetaBOne, phiBOne,
+											                                 thetaATwo, thetaBTwo, phiBTwo);
+	}
+};
+
+template <typename _FP, vectra::SIMDLevel _level>
 class Topology2Sx2SEuclidean : public Topology<_FP, _level, Topology2Sx2SEuclidean<_FP, _level>> {
 
 	template <typename _FPAlias, vectra::SIMDLevel _levelAlias, typename DerivedTopology>
