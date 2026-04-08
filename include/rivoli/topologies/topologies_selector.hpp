a5ce8dafca53051ade1936b33afec9790feec4c5
François Margall <fr.margall@proton.me>
2026-04-08 10:31:56 +0200

    ✨ feat(topologies): add selectors for 3D topologies

--- include/rivoli/topologies/topologies_selector.hpp
+++ include/rivoli/topologies/topologies_selector.hpp
@@ -31,6 +31,52 @@ struct TopologyTraits<2, true, false, CoordinateSystem::Spherical, FP, level> {
 
 
 template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<3, false, false, CoordinateSystem::Spherical, FP, level> {
+	using type = Topology1Rx2SEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<3, true, false, CoordinateSystem::Spherical, FP, level> {
+	using type = Topology1Rx2SBilateralEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<3, false, true, CoordinateSystem::Spherical, FP, level> {
+	using type = Topology1Rx2SReciprocalEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<3, true, true, CoordinateSystem::Spherical, FP, level> {
+	using type = Topology1Rx2SBilateralReciprocalEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits<3, false, false, CoordinateSystem::Rusinkiewicz, FP, level> {
+	// Fallback to the usual 4D topology, since the
+	// Rusinkiewicz system is also defined as 1Rx2S
+	using type = Topology1Rx2SEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits <3, false, true, CoordinateSystem::Rusinkiewicz, FP, level> {
+	using type = Topology1Rx2SReciprocalRusinkiewiczEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits <3, true, false, CoordinateSystem::Rusinkiewicz, FP, level> {
+	// Rusinkiewicz parameterisation cannot intrinsically force
+	// symmetry, without forcing also the Helmholtz reciprocity
+	static_assert(false, "Rusinkiewicz parameterisation cannot be bilateral symmetrical without being also reciprocal");
+	using type = Topology1Rx2SBilateralReciprocalRusinkiewiczEuclidean<FP, level>;
+};
+
+template <typename FP, vectra::SIMDLevel level>
+struct TopologyTraits <3, true, true, CoordinateSystem::Rusinkiewicz, FP, level> {
+	using type = Topology1Rx2SBilateralReciprocalRusinkiewiczEuclidean<FP, level>;
+};
+
+
+template <typename FP, vectra::SIMDLevel level>
 struct TopologyTraits<4, false, false, CoordinateSystem::Spherical, FP, level> {
 	using type = Topology2Sx2SEuclidean<FP, level>;
 };
